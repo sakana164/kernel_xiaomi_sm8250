ae9a3060ac240a7c297c152c3928472335c83311
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-10-28 10:41:39 +0530

    techpack: display: Import xiaomi modifications from dagu-s-oss
    Change-Id: Ib0696c486c8846340728941c54962a9484b26715
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/sde/sde_encoder.h
+++ techpack/display/msm/sde/sde_encoder.h
@@ -406,6 +406,14 @@ static inline struct sde_kms *sde_encoder_get_kms(struct drm_encoder *drm_enc)
  */
 int sde_encoder_vid_wait_for_active(struct drm_encoder *enc);
 
+bool get_sde_encoder_virt_prepare_kickoff(struct drm_connector *connector);
+
+bool get_sde_encoder_virt_ready_kickoff(struct drm_connector *connector);
+
+void set_sde_encoder_virt_prepare_kickoff(struct drm_connector *connector,bool enable);
+
+void set_sde_encoder_virt_ready_kickoff(struct drm_connector *connector,bool enable);
+
 /**
  * sde_encoder_trigger_early_wakeup - trigger early wake up
  * @drm_enc:    Pointer to drm encoder structure
