a22d07347609d0107ceeafb7f0a88052c8fd0113
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-05-28 18:26:55 +0530

    techpack: display: Import xiaomi modifications from pipa-t-oss
    Change-Id: I2743b96548ce065bab365b66792a2da664ec7965
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- techpack/display/msm/sde/sde_kms.c
+++ techpack/display/msm/sde/sde_kms.c
@@ -3971,17 +3971,6 @@ int sde_kms_handle_recovery(struct drm_encoder *encoder)
 	return sde_encoder_wait_for_event(encoder, MSM_ENC_ACTIVE_REGION);
 }
 
-void sde_kms_kickoff_count(struct sde_kms *sde_kms)
-{
-	int i;
-	struct dsi_display *display = NULL;
-
-	if (sde_kms != NULL) {
-		for (i = 0; i < sde_kms->dsi_display_count; ++i)
-			display = sde_kms->dsi_displays[i];
-	}
-}
-
 void sde_kms_trigger_early_wakeup(struct sde_kms *sde_kms,
 		struct drm_crtc *crtc)
 {
