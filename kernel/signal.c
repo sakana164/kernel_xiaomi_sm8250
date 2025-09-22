f9b21c2a281b1e96a8095d180c76d1e511b3af5c
sakana164 <76257039+sakana164@users.noreply.github.com>
2025-09-22 15:44:30 +0800

    binder: Import Re:Kernel feature
    Signed-off-by: sakana164 <76257039+sakana164@users.noreply.github.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -55,6 +55,10 @@
 #include <asm/unistd.h>
 #include <asm/siginfo.h>
 #include <asm/cacheflush.h>
+#ifdef CONFIG_REKERNEL
+#include <uapi/asm/signal.h>
+#include <../drivers/rekernel/rekernel.h>
+#endif /* CONFIG_REKERNEL */
 #include "audit.h"	/* audit_signal_info() */
 
 /*
@@ -1269,6 +1273,10 @@ int do_send_sig_info(int sig, struct siginfo *info, struct task_struct *p,
 {
 	unsigned long flags;
 	int ret = -ESRCH;
+#ifdef CONFIG_REKERNEL
+	if (sig == SIGKILL || sig == SIGTERM || sig == SIGABRT || sig == SIGQUIT)
+		rekernel_report(SIGNAL, sig, task_tgid_nr(current), current, task_tgid_nr(p), p, false, NULL);
+#endif /* CONFIG_REKERNEL */
 
 	if (lock_task_sighand(p, &flags)) {
 		ret = send_signal(sig, info, p, type);
