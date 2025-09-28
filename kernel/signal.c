17e4fa20d4c8c965fe7d33c114d39f851affaa8b
Cloud_Yun <1770669041@qq.com>
2025-09-28 22:25:18 +0900

    ANDROID: backport some GKI vendor hooks to prepare for millet Co-authored-by: Cloud_Yun <1770669041@qq.com> Signed-off-by: sakana164 <76257039+sakana164@users.noreply.github.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -57,6 +57,8 @@
 #include <asm/cacheflush.h>
 #include "audit.h"	/* audit_signal_info() */
 
+#undef CREATE_TRACE_POINTS
+#include <trace/hooks/signal.h>
 /*
  * SLAB caches for signal bits.
  */
@@ -1270,6 +1272,7 @@ int do_send_sig_info(int sig, struct siginfo *info, struct task_struct *p,
 	unsigned long flags;
 	int ret = -ESRCH;
 
+	trace_android_vh_do_send_sig_info(sig, current, p);
 	if (lock_task_sighand(p, &flags)) {
 		ret = send_signal(sig, info, p, type);
 		unlock_task_sighand(p, &flags);
