Rohail33/Realking_kernel_sm8250

qcacld-3.0: Conditional wlan modifications for K81/K81A/L81A
This commit fixes wifi functionality on sm8250 devices other than
K81, K81A and L81A. The changes introduce modifications to the
wlan driver specifically for these devices.

Guard the wlan code using CONFIG_BOARD macros to ensure it
only applies to the target boards.

Change-Id: I0d9fd2cbb63b6b3d7a195049b0ab4116501c14ed
Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_main.h
@@ -1491,7 +1491,9 @@ struct hdd_adapter {
 	qdf_work_t netdev_features_update_work;
 	qdf_atomic_t gro_disallowed;
 	uint8_t gro_flushed[DP_MAX_RX_THREADS];
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 	qdf_event_t install_key_complete;
+#endif
 	bool delete_in_progress;
 	qdf_atomic_t net_dev_hold_ref_count[NET_DEV_HOLD_ID_MAX];
 #ifdef CFG_SUPPORT_SCAN_EXT_FLAG
@@ -4789,6 +4791,7 @@ void hdd_netdev_update_features(struct hdd_adapter *adapter);
  */
 int hdd_stop_no_trans(struct net_device *dev);
 
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 /**
  * hdd_start_install_key - indicate install key start
  * @adapter: Adapter upon which the command was received
@@ -4808,5 +4811,6 @@ void hdd_start_install_key(struct hdd_adapter *adapter);
  * Return: 0 on success and errno on failure
  */
 int hdd_wait_for_install_key_complete(struct hdd_adapter *adapter);
+#endif /* end #if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU) */
 
 #endif /* end #if !defined(WLAN_HDD_MAIN_H) */

drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
@@ -17672,13 +17672,17 @@ static int wlan_hdd_add_key_sta(struct hdd_adapter *adapter,
 	if (!vdev)
 		return -EINVAL;
 
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 	hdd_start_install_key(adapter);
+#endif
 	errno = wlan_cfg80211_crypto_add_key(vdev, (pairwise ?
 					     WLAN_CRYPTO_KEY_TYPE_UNICAST :
 					     WLAN_CRYPTO_KEY_TYPE_GROUP),
 					     key_index, true);
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 	if (!errno)
 		errno = hdd_wait_for_install_key_complete(adapter);
+#endif
 
 	hdd_objmgr_put_vdev(vdev);
 	if (!errno && adapter->send_mode_change) {

drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -3634,6 +3634,7 @@ void hdd_set_qmi_stats_enabled(struct hdd_context *hdd_ctx)
 }
 #endif
 
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 static void
 hdd_install_key_comp_cb(struct wma_install_key_complete_param *param)
 {
@@ -3677,6 +3678,7 @@ int hdd_wait_for_install_key_complete(struct hdd_adapter *adapter)
 	hdd_debug("exit with status %d", status);
 	return qdf_status_to_os_return(status);
 }
+#endif
 
 int hdd_wlan_start_modules(struct hdd_context *hdd_ctx, bool reinit)
 {
@@ -3799,7 +3801,9 @@ int hdd_wlan_start_modules(struct hdd_context *hdd_ctx, bool reinit)
 			goto psoc_close;
 		}
 
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 		wma_register_install_key_complete_cb(hdd_install_key_comp_cb);
+#endif
 
 		hdd_set_qmi_stats_enabled(hdd_ctx);
 
@@ -6518,7 +6522,9 @@ struct hdd_adapter *hdd_open_adapter(struct hdd_context *hdd_ctx, uint8_t sessio
 	qdf_mutex_create(&adapter->blocked_scan_request_q_lock);
 	qdf_event_create(&adapter->acs_complete_event);
 	qdf_event_create(&adapter->peer_cleanup_done);
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 	qdf_event_create(&adapter->install_key_complete);
+#endif
 	hdd_sta_info_init(&adapter->sta_info_list);
 	hdd_sta_info_init(&adapter->cache_sta_info_list);
 	qdf_atomic_init(&adapter->gro_disallowed);
@@ -6581,7 +6587,9 @@ static void __hdd_close_adapter(struct hdd_context *hdd_ctx,
 	qdf_mutex_destroy(&adapter->blocked_scan_request_q_lock);
 	policy_mgr_clear_concurrency_mode(hdd_ctx->psoc, adapter->device_mode);
 	qdf_event_destroy(&adapter->acs_complete_event);
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 	qdf_event_destroy(&adapter->install_key_complete);
+#endif
 	qdf_event_destroy(&adapter->peer_cleanup_done);
 	hdd_adapter_feature_update_work_deinit(adapter);
 	hdd_cleanup_adapter(hdd_ctx, adapter, rtnl_held);

drivers/staging/qcacld-3.0/core/wma/inc/wma.h
@@ -856,6 +856,7 @@ struct wma_wlm_stats_data {
 };
 #endif
 
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 /**
  * struct wma_install_key_complete_param - parameters for installking
  *   key completion callback
@@ -893,6 +894,7 @@ typedef void (*wma_install_key_complete_cb)(
  *
  */
 void wma_register_install_key_complete_cb(wma_install_key_complete_cb cb);
+#endif
 
 /**
  * struct t_wma_handle - wma context
@@ -1161,7 +1163,9 @@ typedef struct {
 #ifdef WLAN_FEATURE_PKT_CAPTURE
 	bool is_pktcapture_enabled;
 #endif
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 	wma_install_key_complete_cb install_key_complete_cb;
+#endif
 } t_wma_handle, *tp_wma_handle;
 
 /**

drivers/staging/qcacld-3.0/core/wma/src/wma_mgmt.c
@@ -836,6 +836,7 @@ void wma_set_sta_keep_alive(tp_wma_handle wma, uint8_t vdev_id,
 	wmi_unified_set_sta_keep_alive_cmd(wma->wmi_handle, &params);
 }
 
+#if IS_ENABLED(CONFIG_BOARD_ELISH) || IS_ENABLED(CONFIG_BOARD_ENUMA) || IS_ENABLED(CONFIG_BOARD_DAGU)
 void wma_register_install_key_complete_cb(wma_install_key_complete_cb cb)
 {
 	tp_wma_handle wma_handle = cds_get_context(QDF_MODULE_ID_WMA);
@@ -847,6 +848,7 @@ void wma_register_install_key_complete_cb(wma_install_key_complete_cb cb)
 
 	wma_handle->install_key_complete_cb = cb;
 }
+#endif
 
 /*
  * 802.11n D2.0 defined values for "Minimum MPDU Start Spacing":

sakana164/kernel_xiaomi_sm8250

drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_main.h
@@ -1491,7 +1491,9 @@ struct hdd_adapter {
 	qdf_work_t netdev_features_update_work;
 	qdf_atomic_t gro_disallowed;
 	uint8_t gro_flushed[DP_MAX_RX_THREADS];
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 	qdf_event_t install_key_complete;
+#endif
 	bool delete_in_progress;
 	qdf_atomic_t net_dev_hold_ref_count[NET_DEV_HOLD_ID_MAX];
 #ifdef CFG_SUPPORT_SCAN_EXT_FLAG
@@ -4789,6 +4791,7 @@ void hdd_netdev_update_features(struct hdd_adapter *adapter);
  */
 int hdd_stop_no_trans(struct net_device *dev);
 
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 /**
  * hdd_start_install_key - indicate install key start
  * @adapter: Adapter upon which the command was received
@@ -4808,5 +4811,6 @@ void hdd_start_install_key(struct hdd_adapter *adapter);
  * Return: 0 on success and errno on failure
  */
 int hdd_wait_for_install_key_complete(struct hdd_adapter *adapter);
+#endif /* end #if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU) */
 
 #endif /* end #if !defined(WLAN_HDD_MAIN_H) */

drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
@@ -17672,13 +17672,17 @@ static int wlan_hdd_add_key_sta(struct hdd_adapter *adapter,
 	if (!vdev)
 		return -EINVAL;
 
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 	hdd_start_install_key(adapter);
+#endif
 	errno = wlan_cfg80211_crypto_add_key(vdev, (pairwise ?
 					     WLAN_CRYPTO_KEY_TYPE_UNICAST :
 					     WLAN_CRYPTO_KEY_TYPE_GROUP),
 					     key_index, true);
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 	if (!errno)
 		errno = hdd_wait_for_install_key_complete(adapter);
+#endif
 
 	hdd_objmgr_put_vdev(vdev);
 	if (!errno && adapter->send_mode_change) {

drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -3634,6 +3634,7 @@ void hdd_set_qmi_stats_enabled(struct hdd_context *hdd_ctx)
 }
 #endif
 
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 static void
 hdd_install_key_comp_cb(struct wma_install_key_complete_param *param)
 {
@@ -3677,6 +3678,7 @@ int hdd_wait_for_install_key_complete(struct hdd_adapter *adapter)
 	hdd_debug("exit with status %d", status);
 	return qdf_status_to_os_return(status);
 }
+#endif
 
 int hdd_wlan_start_modules(struct hdd_context *hdd_ctx, bool reinit)
 {
@@ -3799,7 +3801,9 @@ int hdd_wlan_start_modules(struct hdd_context *hdd_ctx, bool reinit)
 			goto psoc_close;
 		}
 
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 		wma_register_install_key_complete_cb(hdd_install_key_comp_cb);
+#endif
 
 		hdd_set_qmi_stats_enabled(hdd_ctx);
 
@@ -6518,7 +6522,9 @@ struct hdd_adapter *hdd_open_adapter(struct hdd_context *hdd_ctx, uint8_t sessio
 	qdf_mutex_create(&adapter->blocked_scan_request_q_lock);
 	qdf_event_create(&adapter->acs_complete_event);
 	qdf_event_create(&adapter->peer_cleanup_done);
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 	qdf_event_create(&adapter->install_key_complete);
+#endif
 	hdd_sta_info_init(&adapter->sta_info_list);
 	hdd_sta_info_init(&adapter->cache_sta_info_list);
 	qdf_atomic_init(&adapter->gro_disallowed);
@@ -6581,7 +6587,9 @@ static void __hdd_close_adapter(struct hdd_context *hdd_ctx,
 	qdf_mutex_destroy(&adapter->blocked_scan_request_q_lock);
 	policy_mgr_clear_concurrency_mode(hdd_ctx->psoc, adapter->device_mode);
 	qdf_event_destroy(&adapter->acs_complete_event);
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 	qdf_event_destroy(&adapter->install_key_complete);
+#endif
 	qdf_event_destroy(&adapter->peer_cleanup_done);
 	hdd_adapter_feature_update_work_deinit(adapter);
 	hdd_cleanup_adapter(hdd_ctx, adapter, rtnl_held);

drivers/staging/qcacld-3.0/core/wma/inc/wma.h
@@ -856,6 +856,7 @@ struct wma_wlm_stats_data {
 };
 #endif
 
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 /**
  * struct wma_install_key_complete_param - parameters for installking
  *   key completion callback
@@ -893,6 +894,7 @@ typedef void (*wma_install_key_complete_cb)(
  *
  */
 void wma_register_install_key_complete_cb(wma_install_key_complete_cb cb);
+#endif
 
 /**
  * struct t_wma_handle - wma context
@@ -1161,7 +1163,9 @@ typedef struct {
 #ifdef WLAN_FEATURE_PKT_CAPTURE
 	bool is_pktcapture_enabled;
 #endif
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 	wma_install_key_complete_cb install_key_complete_cb;
+#endif
 } t_wma_handle, *tp_wma_handle;
 
 /**

drivers/staging/qcacld-3.0/core/wma/src/wma_mgmt.c
@@ -836,6 +836,7 @@ void wma_set_sta_keep_alive(tp_wma_handle wma, uint8_t vdev_id,
 	wmi_unified_set_sta_keep_alive_cmd(wma->wmi_handle, &params);
 }
 
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_ELISH) || IS_ENABLED(CONFIG_MACH_XIAOMI_ENUMA) || IS_ENABLED(CONFIG_MACH_XIAOMI_DAGU)
 void wma_register_install_key_complete_cb(wma_install_key_complete_cb cb)
 {
 	tp_wma_handle wma_handle = cds_get_context(QDF_MODULE_ID_WMA);
@@ -847,6 +848,7 @@ void wma_register_install_key_complete_cb(wma_install_key_complete_cb cb)
 
 	wma_handle->install_key_complete_cb = cb;
 }
+#endif
 
 /*
  * 802.11n D2.0 defined values for "Minimum MPDU Start Spacing":